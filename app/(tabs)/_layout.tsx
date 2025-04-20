678c7e7322c17d26f85eb8553dbcbc7b2522c071
Phong Do <Donguyenphong@icloud.com>
2025-04-19 17:26:09 -0700

    Basic edit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -34,6 +34,12 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        }}
+      /><Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
